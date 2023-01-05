c6195da48e2bc95ee839254120e756dce823d402
David Tonoyan <davidtonoyan10@me.com>
2023-01-06 01:45:08 +0400

    Fix scene delegate

--- filmLibrary/Sources/SceneDelegate.swift
+++ filmLibrary/Sources/SceneDelegate.swift
@@ -9,14 +9,35 @@ import UIKit
 
 final class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
-
+    
     func scene(_ scene: UIScene,
                willConnectTo session: UISceneSession,
                options connectionOptions: UIScene.ConnectionOptions) {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let window = UIWindow(windowScene: windowScene)
-        // root
+        
+        let tabBarController = UITabBarController()
+        
+        let navigationController = UINavigationController(rootViewController: MainScreenViewController())
+        navigationController.navigationBar.prefersLargeTitles = true
+        navigationController.navigationBar.largeTitleTextAttributes = [
+            NSAttributedString.Key.foregroundColor: Constants.Color.white
+        ]
+        navigationController.navigationBar.tintColor = Constants.Color.white
+        
+        let mainScreen = navigationController
+        let mainScreenTabBarItem = UITabBarItem(title: "Main",
+                                                image: UIImage(systemName: "film.stack"),
+                                                selectedImage: UIImage(systemName: "film.stack.fill"))
+        mainScreen.tabBarItem = mainScreenTabBarItem
+        
+        tabBarController.tabBar.tintColor = Constants.Color.blue
+        tabBarController.tabBar.backgroundColor = Constants.Color.white.withAlphaComponent(0.5)
+        tabBarController.viewControllers = [mainScreen]
+        
+        window.rootViewController = tabBarController
+        
         window.makeKeyAndVisible()
         self.window = window
     }
