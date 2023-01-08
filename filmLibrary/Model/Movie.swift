44448a650f611652a6ea5e2b7061702919d7aadc
David Tonoyan <davidtonoyan10@me.com>
2023-01-08 16:17:14 +0400

    Fix movie struct

--- filmLibrary/Model/Movie.swift
+++ filmLibrary/Model/Movie.swift
@@ -7,7 +7,35 @@
 
 import Foundation
 
-// MARK: - Enum types
+// MARK: - PopularMovies
+struct PopularMovies: Decodable {
+    let movies: [Movie]
+}
+
+// MARK: - Movie
+struct Movie: Decodable {
+    let poster: Poster
+    let rating: Rating
+    let movieLength, id: Int
+    let type: MovieType
+    let name, description: String
+    let year: Int
+    let shortDescription: String?
+}
+
+// MARK: - Poster
+struct Poster: Decodable {
+    let previewUrl: String
+}
+
+// MARK: - Rating
+struct Rating: Decodable {
+    let kinopoisk: Double
+    
+    enum CodingKeys: String, CodingKey {
+        case kinopoisk = "kp"
+    }
+}
 
 enum MovieType: String, Decodable {
     case movie = "movie"
@@ -16,99 +44,4 @@ enum MovieType: String, Decodable {
     case anime = "anime"
     case animatedSeries = "animated-series"
     case tvShow = "tv-show"
-}
-
-enum SexType: String, Decodable {
-    case men = "Мужской"
-    case women = "Женский"
-}
-
-enum ReviewType: String, Decodable {
-    case positive = "Положительный"
-    case negative = "Негативный"
-}
-
-// MARK: - Global struct models
-
-struct Movie: Decodable {
-    let id: Int
-    let name: String
-    let type: MovieType
-    let description: String
-    let year: Int
-    let movieLength: Int
-    let rating: Rating
-    let poster: Poster
-    let genres: [Genre]
-    let facts: [Fact]
-    //let persons: [ShortPerson]
-}
-
-struct Person: Decodable {
-    let id: Int
-    let name: String
-    let photo: String
-    let sex: SexType
-    let growth: Int
-    let birthday: String
-    let death: String
-    let age: Int
-    let profession: Profession
-    let facts: [Fact]
-    let movies: [ShortMovie]
-}
-
-struct Review: Decodable {
-    let id: Int
-    let movieId: Int
-    let title: String
-    let type: ReviewType
-    let review: String
-    let date: String
-    let author: String
-}
-
-// MARK: - Struct helpers
-
-struct Rating: Decodable {
-    let kp: Double?
-    let tmdb: Double?
-    let imdb: Double?
-}
-
-struct Poster: Decodable {
-    let url: String
-    let previewUrl: String
-}
-
-struct Budget: Decodable {
-    let value: String
-    let currency: String
-}
-
-struct Genre: Decodable {
-    let name: String
-}
-
-struct Fact: Decodable {
-    let value: String
-}
-
-struct Profession: Decodable {
-    let value: String
-}
-
-struct ShortMovie: Decodable {
-    let id: Int
-    let name: String
-    let rating: Double
-    let description: String
-}
-
-struct ShortPerson: Decodable {
-    let id: Int
-    let name: String
-    let photo: String
-    let enProfession: String
-    let personDescription: String?
 }
