b5c602c372c2869bda074a79c51fb8c2bad47078
David Tonoyan <davidtonoyan10@me.com>
2023-01-08 16:12:15 +0400

    Add imageView extension to setup image

---
+++ filmLibrary/Extensions/ImageView.swift
@@ -0,0 +1,29 @@
+//
+//  ImageView.swift
+//  filmLibrary
+//
+//  Created by Давид Тоноян  on 07.01.2023.
+//
+
+import UIKit
+import Kingfisher
+
+extension UIImageView {
+    func setupImage(with imageUrl: String) {
+        DispatchQueue.main.async { [weak self] in
+            self?.tintColor = Constants.Color.orange
+            let url = URL(string: imageUrl)
+            let processor = DownsamplingImageProcessor(size: self?.bounds.size ?? .zero)
+            self?.kf.indicatorType = .activity
+            self?.kf.setImage(
+                with: url,
+                placeholder: UIImage(systemName: "photo"),
+                options: [
+                    .processor(processor),
+                    .scaleFactor(UIScreen.main.scale),
+                    .transition(.fade(1)),
+                    .cacheOriginalImage
+                ])
+        }
+    }
+}
