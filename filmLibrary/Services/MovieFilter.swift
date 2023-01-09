b85dc9ab8c253426e985b17e0a54403142df8aae
David Tonoyan <davidtonoyan10@me.com>
2023-01-09 18:04:50 +0400

    Add MovieFilter class

---
+++ filmLibrary/Services/MovieFilter.swift
@@ -0,0 +1,16 @@
+//
+//  MovieFilter.swift
+//  filmLibrary
+//
+//  Created by Давид Тоноян  on 09.01.2023.
+//
+
+import Foundation
+
+protocol MovieFilterProtocol {
+    
+}
+
+class MovieFilter {
+    
+}
